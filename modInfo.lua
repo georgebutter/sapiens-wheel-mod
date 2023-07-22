adc20fa82a4680abad41a2163c4ed1b8d568e587
George Butter <georgestuartbutter@gmail.com>
2023-07-22 15:13:19 +1000

    feat: init

---
+++ modInfo.lua
@@ -0,0 +1,9 @@
+local modInfo = {
+  name = "Wheels",
+  description = "Sapiens greatest invention... so far.",
+  preview = "preview.png",
+  type = "app",
+  developer = "Buts",
+  website = "https://github.com/georgebutter/sapiens-wheel-mod"
+}
+return modInfo
